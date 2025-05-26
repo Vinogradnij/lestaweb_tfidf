1a759c0b77478bbb31b02dd4adb2226b86122c5b
Vinogradnij <Vlud5@mail.ru>
2025-05-26 19:52:39 +0300

    Обновлены примеры для документации

--- src/schemas/tf_idf.py
+++ src/schemas/tf_idf.py
@@ -10,18 +10,32 @@ class OutputResults(BaseModel):
         'json_schema_extra': {
             'examples': [
                 {
-                    'results': [
-                        {
-                            'name': 'предвидеть',
-                            'tf': ['0.00585 для файла OneginKTatyane.txt'],
-                            'idf': '0.30103'
-                         },
-                        {
-                            'name': 'оскорбить',
-                            'tf': ['0.00585 для файла OneginKTatyane.txt'],
-                            'idf': '0.30103'
-                         },
-                    ],
+                        'results': [
+                            {
+                                'name': 'word1',
+                                'tf': [
+                                    '0.33333 для файла Test1.txt'
+                                ],
+                                'idf': '0.47712'
+                            },
+                            {
+                                'name': 'word2',
+                                'tf': [
+                                    '0.33333 для файла Test1.txt',
+                                    '0.5 для файла Test2.txt'
+                                ],
+                                'idf': '0.17609'
+                            },
+                            {
+                                'name': 'word3',
+                                'tf': [
+                                    '0.33333 для файла Test1.txt',
+                                    '1.0 для файла Test3.txt',
+                                    '0.5 для файла Test2.txt'
+                                ],
+                                'idf': '0.0'
+                            }
+                        ]
                 }
             ]
         }
