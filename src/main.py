8921ca992f49f3057a59137d08f112631dc08c5c
Vinogradnij <Vlud5@mail.ru>
2025-06-02 00:57:29 +0300

    Создан lifespan для главного приложения, добавлено действие при выключении - закрытие БД (engine.dispose)

--- src/main.py
+++ src/main.py
@@ -1,11 +1,18 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
 
+from database import db_handler
 from tfidf.router import router as api_router
 from config import settings
 
 app = FastAPI()
 app.include_router(
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_handler.dispose()
+
     api_router,
     prefix=settings.api.prefix
 )
