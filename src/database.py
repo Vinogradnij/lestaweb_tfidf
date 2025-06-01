650214268137aefcd204bc8d6eaa9ae5706469f8
Vinogradnij <Vlud5@mail.ru>
2025-06-02 00:54:09 +0300

    Исправлена опечатка в коде

--- src/database.py
+++ src/database.py
@@ -15,7 +15,7 @@ class DatabaseHandler:
     ):
         self.engine: AsyncEngine = create_async_engine(
             url=url,
-            ehco=echo,
+            echo=echo,
             echo_pool=echo_pool,
             pool_size=pool_size,
             max_overflow=max_overflow,
