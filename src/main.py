3ce800b5a82464caf694be100cce41fd5125c037
Vinogradnij <Vlud5@mail.ru>
2025-06-02 00:58:01 +0300

    Экземпляр приложения app переименован в app_main

--- src/main.py
+++ src/main.py
@@ -6,20 +6,22 @@ from database import db_handler
 from tfidf.router import router as api_router
 from config import settings
 
-app = FastAPI()
-app.include_router(
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
     await db_handler.dispose()
 
+app_main = FastAPI(
+    lifespan=lifespan,
+)
+app_main.include_router(
     api_router,
     prefix=settings.api.prefix
 )
 
 if __name__ == '__main__':
     uvicorn.run(
-        'main:app',
+        'main:app_main',
         host=settings.run.host,
         port=settings.run.port,
         reload=True,
