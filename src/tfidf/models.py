1f25853824d773377120341feabac756be519fa0
Vinogradnij <Vlud5@mail.ru>
2025-06-02 14:14:12 +0300

    Удаление модели для записей метрик

--- src/tfidf/models.py
+++ src/tfidf/models.py
@@ -4,9 +4,4 @@ from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
 
-class Records(Base):
-    files_processed: Mapped[int]
-    min_time_processed: Mapped[float]
-    avg_time_processed: Mapped[float]
-    max_time_processed: Mapped[float]
-    latest_file_processed_timestamp = mapped_column(TIMESTAMP, nullable=False)
+
