a9ac2cf3a20cf512486d453054d73931ce2a25b5
Vinogradnij <Vlud5@mail.ru>
2025-06-02 14:15:56 +0300

    Для получения метрик приложения поставлена заглушка, реализующая вывод нулевых значений метрик

--- src/tfidf/service.py
+++ src/tfidf/service.py
@@ -1,15 +1,12 @@
 from datetime import datetime
 
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from tfidf.schemas import MetricsOut
 
 
-async def get_metrics_tfidf(session: AsyncSession) -> RecordsOut:
-    stmt = select(Records)
-    result = await session.scalar(stmt)
 async def get_metrics_tfidf(session: AsyncSession) -> MetricsOut:
+    result = None
     if not result:
         result = MetricsOut(
             files_processed=0,
@@ -18,6 +15,5 @@ async def get_metrics_tfidf(session: AsyncSession) -> MetricsOut:
             max_time_processed=0,
             latest_file_processed_timestamp=datetime.min,
         )
-    else:
-        result = RecordsOut(**Records.__dict__)
+
     return result
