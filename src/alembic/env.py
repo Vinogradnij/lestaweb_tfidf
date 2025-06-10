532e2ed951973ca50a522c0966f33e04c597d555
Vinogradnij <Vlud5@mail.ru>
2025-06-11 01:23:25 +0300

    edit(alembic): rebase alembic

--- src/alembic/env.py
+++ src/alembic/env.py
@@ -7,9 +7,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from src.database import Base
-from src.config import settings
-from src.users.models import User
+from database import Base
+from config import settings
+from users.models import User
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
