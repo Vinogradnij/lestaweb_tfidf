50f670f480067d6fb4c2c376372801174cddf49d
Vinogradnij <Vlud5@mail.ru>
2025-06-11 01:24:49 +0300

    edit(models): edit import

--- src/users/models.py
+++ src/users/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import Mapped, mapped_column
 
-from src.database import Base
+from database import Base
 
 class User(Base):
     login: Mapped[str] = mapped_column(unique=True)
