b619d5ab1c6dec88e3d737b613eae922baa7886a
Vinogradnij <Vlud5@mail.ru>
2025-06-14 20:13:14 +0300

    move(dependencies): move dep's from users to src

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,8 +1,10 @@
 from typing import Annotated
 
 from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import db_handler
+from users.schemas import UserBase
 
 session_dep = Annotated[AsyncSession, Depends(db_handler.session_dep)]
