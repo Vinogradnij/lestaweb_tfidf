756831eb92df4517270b1ae62295f370a12b293f
Vinogradnij <Vlud5@mail.ru>
2025-06-15 02:53:26 +0300

    add(schemas): add schemas for Token

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -8,5 +8,15 @@ class UserBase(BaseModel):
 class UserInDb(UserBase):
     hashed_password: str
 
+
 class UserPassword(UserBase):
     password: str
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str | None = None
