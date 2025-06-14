601efa513d7b911e34d6f3a7ad2f12dc422dbbf6
Vinogradnij <Vlud5@mail.ru>
2025-06-15 02:11:26 +0300

    create(utils): create def auth_user

--- src/users/utils.py
+++ src/users/utils.py
@@ -2,6 +2,7 @@ from passlib.context import CryptContext
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from users.crud import get_user_by_username
+from users.schemas import UserBase
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated='auto')
 
@@ -17,3 +18,11 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
 async def check_user_in_db(session: AsyncSession, username: str) -> bool:
     user_in_db = await get_user_by_username(session=session, username=username)
     return True if user_in_db else False
+
+async def auth_user(session: AsyncSession, username: str, password: str) -> UserBase | bool:
+    user = await get_user_by_username(session=session, username=username)
+    if not user:
+        return False
+    if not verify_password(password, user.password):
+        return False
+    return UserBase(username=user.username)
