499b90c24eeda92ba452f2cdad56387d640287ec
Vinogradnij <Vlud5@mail.ru>
2025-06-15 01:27:46 +0300

    add(utils): add def check user in db

--- src/users/router.py
+++ src/users/router.py
@@ -2,7 +2,8 @@ from fastapi import APIRouter,HTTPException, status
 
 from dependencies import session_dep
 from users.schemas import UserCreate, UserLogout
-from users.crud import create_user, get_user_by_username
+from users.crud import create_user
+from users.utils import check_user_in_db
 
 router = APIRouter(
     tags=['Пользователи'],
@@ -28,8 +29,8 @@ async def register(
         user_in: UserCreate,
         session: session_dep,
 ):
-    user_in_db = await get_user_by_username(session=session, username=user_in.username)
-    if user_in_db:
+    check_user = check_user_in_db(session=session, username=user_in.username)
+    if check_user:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='User already exists')
     user = await create_user(session=session, user_in=user_in)
     return user
--- src/users/utils.py
+++ src/users/utils.py
@@ -1,4 +1,7 @@
 from passlib.context import CryptContext
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from users.crud import get_user_by_username
 
 pwd_context = CryptContext(schemes=["bcrypt"])
 
@@ -9,3 +12,8 @@ def hash_password(password: str) -> str:
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
+
+
+async def check_user_in_db(session: AsyncSession, username: str) -> bool:
+    user_in_db = await get_user_by_username(session=session, username=username)
+    return True if user_in_db else False
