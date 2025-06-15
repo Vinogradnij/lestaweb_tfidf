9853de5cfed59c9c65577d28cf39e8a96bc35efb
Vinogradnij <Vlud5@mail.ru>
2025-06-15 19:27:26 +0300

    create(mixins): UserRelationMixin

--- src/tfidf/models.py
+++ src/tfidf/models.py
@@ -4,14 +4,15 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
 from database import Base
+from tfidf.mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from users.models import User
 
 
-class Document(Base):
-    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+class Document(UserRelationMixin, Base):
+    _user_back_populates = 'documents'
+
     path: Mapped[str]
     title: Mapped[str]
 
-    user: Mapped['User'] = relationship(back_populates='documents')
