7c404bef77248dddcc798c2e387dd3714fc77351
Vinogradnij <Vlud5@mail.ru>
2025-06-15 03:33:53 +0300

    edit(dependencies): edit token url for oauth2_scheme

--- src/dependencies.py
+++ src/dependencies.py
@@ -8,7 +8,9 @@ from database import db_handler
 from users.schemas import UserBase
 
 session_dep = Annotated[AsyncSession, Depends(db_handler.session_dep)]
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 token_dep = Annotated[str, Depends(oauth2_scheme)]
 
 form_data_dep = Annotated[OAuth2PasswordRequestForm, Depends()]
+
+auth_dep = Annotated[UserBase, Depends()]
