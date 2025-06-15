02dc961dbfab0dcd428897d992041f30401aea99
Vinogradnij <Vlud5@mail.ru>
2025-06-15 19:12:45 +0300

    add(models): add relationship from User to Document, from Document to User

--- src/tfidf/models.py
+++ src/tfidf/models.py
@@ -1,12 +1,17 @@
-from sqlalchemy import TIMESTAMP
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
 from database import Base
 
+if TYPE_CHECKING:
+    from users.models import User
 
 
 class Document(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
     path: Mapped[str]
     title: Mapped[str]
+
+    user: Mapped['User'] = relationship(back_populates='documents')
--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,14 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from database import Base
+
+if TYPE_CHECKING:
+    from tfidf.models import Document
 
 class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
+
+    documents: Mapped[list['Document']] = relationship(back_populates='user')
