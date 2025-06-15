835e0c18429b4483a1dbd6e8c2f821e3b9653e26
Vinogradnij <Vlud5@mail.ru>
2025-06-15 20:25:34 +0300

    add(models): CollectionDocument table

--- src/tfidf/models.py
+++ src/tfidf/models.py
@@ -2,7 +2,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy import ForeignKey
 
 from database import Base
-from tfidf.mixins import UserRelationMixin
+from tfidf.mixins import UserRelationMixin, DocumentRelationMixin, CollectionRelationMixin
 
 
 
@@ -14,3 +14,8 @@ class Document(UserRelationMixin, Base):
 
 class Collection(UserRelationMixin, Base):
     _user_back_populates = 'collections'
+
+
+class CollectionDocument(DocumentRelationMixin, CollectionRelationMixin, Base):
+    _document_back_populates = 'collection_document'
+    _collection_back_populates = 'collection_document'
