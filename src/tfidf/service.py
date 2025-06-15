1c2a0d3eb5faa5f205778771a1f1df2fd5e5d184
Vinogradnij <Vlud5@mail.ru>
2025-06-15 18:51:47 +0300

    move(service): move get_metrics from tfidf to info

--- src/tfidf/service.py
+++ src/tfidf/service.py
@@ -1,19 +1,1 @@
-from datetime import datetime
 
-from sqlalchemy.ext.asyncio import AsyncSession
-
-from tfidf.schemas import MetricsOut
-
-
-async def get_metrics_tfidf(session: AsyncSession) -> MetricsOut:
-    result = None
-    if not result:
-        result = MetricsOut(
-            files_processed=0,
-            min_time_processed=0,
-            avg_time_processed=0,
-            max_time_processed=0,
-            latest_file_processed_timestamp=datetime.min,
-        )
-
-    return result
