322fc98d85979cd85aa4683bf079b41964dbbf5c
Vinogradnij <Vlud5@mail.ru>
2025-06-15 03:32:39 +0300

    add(dependencies): add form_data_dep

--- src/dependencies.py
+++ src/dependencies.py
@@ -10,3 +10,5 @@ from users.schemas import UserBase
 session_dep = Annotated[AsyncSession, Depends(db_handler.session_dep)]
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 token_dep = Annotated[str, Depends(oauth2_scheme)]
+
+form_data_dep = Annotated[OAuth2PasswordRequestForm, Depends()]
