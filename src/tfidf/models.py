c53cec15bef0f97174fc4448600b7895bf297be5
Vinogradnij <Vlud5@mail.ru>
2025-06-15 19:11:58 +0300

    create(models): create Document model

--- src/tfidf/models.py
+++ src/tfidf/models.py
@@ -1,7 +1,12 @@
 from sqlalchemy import TIMESTAMP
 from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
 
 from database import Base
 
 
 
+class Document(Base):
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.id'))
+    path: Mapped[str]
+    title: Mapped[str]
