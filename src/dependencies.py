3b62f4130d2f4cb523f1fc3333fbb71e142bc9a8
Vinogradnij <Vlud5@mail.ru>
2025-06-15 05:33:30 +0300

    delete(schemas): delete oauth2

--- src/dependencies.py
+++ src/dependencies.py
@@ -1,15 +1,11 @@
 from typing import Annotated
 
-from fastapi import Depends
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from fastapi import Depends, Request, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from database import db_handler
-from users.schemas import UserBase
 
 session_dep = Annotated[AsyncSession, Depends(db_handler.session_dep)]
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
-token_dep = Annotated[str, Depends(oauth2_scheme)]
 
 form_data_dep = Annotated[OAuth2PasswordRequestForm, Depends()]
 
