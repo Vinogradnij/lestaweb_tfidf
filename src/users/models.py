b1e742cf3d0c139daba9d5cfaa835a6db2e0e603
Vinogradnij <Vlud5@mail.ru>
2025-06-15 20:15:19 +0300

    add(models): relationship between User, Collection

--- src/users/models.py
+++ src/users/models.py
@@ -5,10 +5,11 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base
 
 if TYPE_CHECKING:
-    from tfidf.models import Document
+    from tfidf.models import Document, Collection
 
 class User(Base):
     username: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
 
     documents: Mapped[list['Document']] = relationship(back_populates='user')
+    collections: Mapped[list['Collection']] = relationship(back_populates='user')
