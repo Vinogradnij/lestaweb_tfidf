444b4032e64d9c4b321735f68c66301f46e3c638
Vinogradnij <Vlud5@mail.ru>
2025-06-15 03:29:41 +0300

    fix(crud): fix create user

--- src/users/crud.py
+++ src/users/crud.py
@@ -14,11 +14,11 @@ from config import settings
 
 async def create_user(session: AsyncSession, user_in: UserPassword) -> UserInDb:
     hashed_pass = hash_password(user_in.password)
-    user = UserInDb(username=user_in.username, hashed_password=hashed_pass)
+    user = User(username=user_in.username, password=hashed_pass)
     session.add(user)
     await session.commit()
     await session.refresh(user)
-    return user
+    return UserInDb(username=user.username, hashed_password=user.password)
 
 async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
     result = await session.execute(select(User).where(User.username == username))
