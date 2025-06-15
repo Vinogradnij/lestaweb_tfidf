6522367ba1da14fbf8774844a80064c3db3a9a00
Vinogradnij <Vlud5@mail.ru>
2025-06-15 21:02:52 +0300

    add(router): route's for documents and collections

--- src/tfidf/router.py
+++ src/tfidf/router.py
@@ -37,3 +37,75 @@ async def upload_files(files: list[UploadFile]) -> OutputResults:
 
     results = compute_tfidf(files)
     return OutputResults(results=results)
+
+
+@router.get(
+    '/documents',
+    summary='Получить список документов пользователя'
+)
+async def get_documents():
+    pass
+
+
+@router.get(
+    '/documents/{document_id}',
+    summary='Получить содержимое документа'
+)
+async def get_document():
+    pass
+
+
+@router.get(
+    '/documents/{document_id}/statistics',
+    summary='Получить статистику по данному документу'
+)
+async def get_document_statistics():
+    pass
+
+
+@router.delete(
+    '/documents/{document_id}',
+    summary='Удалить документ'
+)
+async def delete_document():
+    pass
+
+
+@router.get(
+    '/collections',
+    summary='Получить список коллекций и список входящих в них документов'
+)
+async def get_collections():
+    pass
+
+
+@router.get(
+    '/collections/{collection_id}',
+    summary='Получить список входящих в коллекцию id документов'
+)
+async def get_collection():
+    pass
+
+
+@router.get(
+    '/collections/{collection_id}/statistics',
+    summary='Получить статистику по коллекции'
+)
+async def get_collection_statistics():
+    pass
+
+
+@router.post(
+    '/collections/{collection_id}/{document_id}',
+    summary='Добавить документ в коллекцию'
+)
+async def add_document():
+    pass
+
+
+@router.delete(
+    '/collections/{collection_id}/{document_id}',
+    summary='Удалить документ из коллекции'
+)
+async def add_document():
+    pass
