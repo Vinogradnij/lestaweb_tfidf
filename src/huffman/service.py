e40463a20fd684cccb9c8eb61a12a608d7be6dd1
Vinogradnij <Vlud5@mail.ru>
2025-06-18 07:45:51 +0300

    add(service) - def encode

--- src/huffman/service.py
+++ src/huffman/service.py
@@ -1,7 +1,9 @@
+from collections import deque
+
 import aiofiles
 
 
-async def get_frequency(path):
+async def get_frequency(path: str):
     fq: dict[str, int] = {}
     async with aiofiles.open(path, 'r', encoding='utf-8') as file:
         while chunks := await file.read(1024):
@@ -11,3 +13,12 @@ async def get_frequency(path):
                 else:
                     fq[chunk] = 1
     return fq
+
+
+async def encode(path: str, encoding: dict[str, str]) -> str:
+    text = deque()
+    async with aiofiles.open(path, 'r', encoding='utf-8') as file:
+        while chunks := await file.read(1024):
+            for chunk in chunks:
+                text.append(encoding[chunk])
+    return ''.join(text)
