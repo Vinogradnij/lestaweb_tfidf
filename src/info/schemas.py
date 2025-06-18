23e71c3ff96a7caf70fa9495acf0319dad0f0894
Vinogradnij <Vlud5@mail.ru>
2025-06-18 10:12:48 +0300

    add(schemas) - new fields for MetricsOut

--- src/info/schemas.py
+++ src/info/schemas.py
@@ -1,5 +1,3 @@
-from datetime import datetime
-
 from pydantic import BaseModel
 
 
@@ -8,4 +6,12 @@ class MetricsOut(BaseModel):
     min_time_processed: float
     avg_time_processed: float
     max_time_processed: float
-    latest_file_processed_timestamp: datetime
+    all_time_processed: float
+    latest_file_processed_timestamp: float
+
+    files_huffman: int
+    min_time_huffman: float
+    avg_time_huffman: float
+    max_time_huffman: float
+    all_time_huffman: float
+    latest_huffman: float
