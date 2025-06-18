af7482245af4fa53c9dbd9236a04094cf95ca39f
Vinogradnij <Vlud5@mail.ru>
2025-06-18 10:43:53 +0300

    fix(crud) - add zero-check for metrics.min_time

--- src/info/crud.py
+++ src/info/crud.py
@@ -16,8 +16,11 @@ async def add_metrics(
 
     if not for_huffman:
         metrics.files_processed += number_of_files
-        metrics.min_time_processed = metrics.min_time_processed \
-            if metrics.min_time_processed < duration else duration
+        if metrics.min_time_processed == 0:
+            metrics.min_time_processed = duration
+        else:
+            metrics.min_time_processed = metrics.min_time_processed \
+                if metrics.min_time_processed < duration else duration
         metrics.all_time_processed += duration
         metrics.avg_time_processed = metrics.avg_time_processed \
             if metrics.files_processed == 0 else metrics.all_time_processed / metrics.files_processed
@@ -26,8 +29,11 @@ async def add_metrics(
         metrics.latest_file_processed_timestamp=(datetime.now(UTC) - timedelta(seconds=duration)).timestamp()
     else:
         metrics.files_huffman += number_of_files
-        metrics.min_time_huffman = metrics.min_time_huffman \
-            if metrics.min_time_huffman < duration else duration
+        if metrics.min_time_huffman == 0:
+            metrics.min_time_huffman = duration
+        else:
+            metrics.min_time_huffman = metrics.min_time_huffman \
+                if metrics.min_time_huffman < duration else duration
         metrics.all_time_huffman += duration
         metrics.avg_time_huffman = metrics.avg_time_huffman \
             if metrics.files_huffman == 0 else metrics.all_time_huffman / metrics.files_huffman
